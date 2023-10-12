888034cf737e04e0ffdd5c7d69a944c46c360bc6
elroy <devinjl665@gmail.com>
2023-10-12 14:59:22 -0500

    Added prompts for each character type

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,18 @@ function generatePassword() {
     passwordLength = parseInt(prompt("Enter the password length (between 10 and 128 characters):"));
   } while (isNaN(passwordLength) || passwordLength < 10 || passwordLength > 128);
 
+  // Prompt for character types
+  var useLowercase = confirm("Use lowercase characters?");
+  var useUppercase = confirm("Use uppercase characters?");
+  var useNumeric = confirm("Use numeric characters?");
+  var useSpecial = confirm("Use special characters?");
 
+  var possibleChars = "";
+  if (useLowercase) possibleChars += lowercaseChars;
+  if (useUppercase) possibleChars += uppercaseChars;
+  if (useNumeric) possibleChars += numericChars;
+  if (useSpecial) possibleChars += specialChars;
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
