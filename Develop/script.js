d3e81c942d21d63ed65693413f8542ae7d3d94a8
elroy <devinjl665@gmail.com>
2023-10-12 13:32:44 -0500

    Added function for password length, uppercase, lowercase, numbers, and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Function to generate a random password
+function generatePassword() {
+  var passwordLength;
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_-+=<>?";
+
+// Prompt for the password length
+  do {
+    passwordLength = parseInt(prompt("Enter the password length (between 10 and 128 characters):"));
+  } while (isNaN(passwordLength) || passwordLength < 10 || passwordLength > 128);
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +26,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+}
